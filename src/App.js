639314c6b14ae435b499c91527260de5622b80f8
Hyojeon Yoon <se1620236@naver.com>
2021-02-24 06:12:49 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,123 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Subject from './components/Subject';
+import TOC from './components/TOC';
+import ReadContent from './components/ReadContent';
+import CreateContent from './components/CreateContent';
+import UpdateContent from './components/UpdateContent';
+import Control from './components/Control';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.max_content_id = 3;
+    this.state = {
+      mode:'welcome',
+      selected_content_id:2,
+      subject:{ title: 'WEB', sub:'world wide web!' },
+      welcome:{ title:'Welcome', desc:'Hello, React!!' },
+      contents:[
+        { id:1, title:'HTML', desc:'HTML is for information' },
+        { id:2, title:'CSS', desc:'CSS is for design' },
+        { id:3, title:'JavaScript', desc:'JavaScript is for interactive' },
+      ],
+    };
+  }
+
+  getReadContent() {
+    let i = 0;
+    while(i < this.state.contents.length) {
+      let data = this.state.contents[i];
+      if(data.id === this.state.selected_content_id) {
+        return data;
+      }
+      i = i + 1;
+    }
+  }
+
+  getContent() {
+    let _title, _desc, _article = null;
+
+    if(this.state.mode === 'welcome') {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>;
+    }
+    else if(this.state.mode === 'read') {
+      let _content = this.getReadContent();
+      _article = <ReadContent title={_content.title} desc={_content.desc}></ReadContent>;
+    }
+    else if(this.state.mode === 'create') {
+      _article = <CreateContent onSubmit={(_title, _desc) => {
+        this.max_content_id = this.max_content_id + 1;
+        let _contents = Array.from(this.state.contents);
+        _contents.push({id:this.max_content_id, title:_title, desc:_desc});
+        this.setState({contents:_contents});
+      }}></CreateContent>;
+    }
+    else if(this.state.mode === 'update') {
+      let _content = this.getReadContent();
+      _article = <UpdateContent
+                    data={_content}
+                    onSubmit={(_id, _title, _desc) => {
+                      let _contents = Array.from(this.state.contents);
+                      let i = 0;
+                      while(i < _contents.length) {
+                        if(_contents[i].id === _id) {
+                          _contents[i] = {id:_id, title:_title, desc:_desc};
+                          break;
+                        }
+                        i = i + 1;
+                      }
+                      this.setState({contents:_contents, mode:'read'});
+                    }}
+                  />;
+    }
+    return _article;
+  }
+
+  render() {
+    console.log('App render');
+  
+    return (
+      <div className="App">
+        <Subject
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}
+          onChangePage={() => {
+            this.setState({mode:'welcome'});
+          }}
+        ></Subject>
+        <TOC
+          onChangePage={(id) => {
+            this.setState({mode:'read', selected_content_id:Number(id)});
+          }}
+          data={this.state.contents}
+        ></TOC>
+        <Control onChangeMode={(_mode) => {
+          if(_mode === 'delete') {
+            if(window.confirm('really?')) {
+              let _contents = Array.from(this.state.contents);
+              let i = 0;
+              while(i < this.state.contents.length) {
+                if(_contents[i].id === this.state.selected_content_id) {
+                  _contents.splice(i, 1);
+                  break;
+                }
+                i = i + 1;
+              }
+              this.setState({mode:'welcome', contents:_contents});
+              alert('deleted!');
+            }
+          }
+          else {
+            this.setState({mode:_mode});
+          }
+        }}/>
+        {this.getContent()}
+      </div>
+    );
+  }
 }
 
 export default App;
